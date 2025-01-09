a0acd1f49951b32035e6dfb4169a7f6270b2f969
svirov <sania.svirov@gmail.com>
2025-01-09 16:53:08 +0200

    Fix DB

--- docker-entrypoint-initdb.d/example.sql
+++ docker-entrypoint-initdb.d/example.sql
@@ -236,27 +236,6 @@ LOCK TABLES `failed_jobs` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `laravel`
---
-
-DROP TABLE IF EXISTS `laravel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `laravel` (
-  `C1` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `laravel`
---
-
-LOCK TABLES `laravel` WRITE;
-/*!40000 ALTER TABLE `laravel` DISABLE KEYS */;
-/*!40000 ALTER TABLE `laravel` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `main_banners`
 --
 
